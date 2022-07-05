fa91e07f88eef024edfbcb51dfefc2ededda05cd
JackArchibald <jwarchibald@wisc.edu>
2022-07-04 23:16:17 -0400

    Updated About Me Button

--- aboutme.js
+++ aboutme.js
@@ -1,11 +1,10 @@
-var messageArray = ["Jack Archibald"]
+var messageArray = ["Jack Archibald - UW Madison Computer Sciences"]
 var textPosition = 0
 var speed = 100
 typewriter = () => {
     document.querySelector("#message").
     innerHTML = messageArray[0].substring(0,textPosition) +
     "<span>\u25ae</span>"
-
     if(textPosition++ != messageArray[0].length){
         setTimeout(typewriter, speed);
     }
