8230225c4822eddc53127810821b0cba8620121a
jackabald <jwarchibald@wisc.edu>
2024-04-16 11:26:30 -0500

    added typerwriter effect

--- aboutme.js
+++ aboutme.js
@@ -1,13 +1,12 @@
-
-var messageArray = ["Jack Archibald - UW Madison Computer Science"]
-var textPosition = 0
-var speed = 100
+// Creates typewriter effect on AboutMe page.
+var messageArray = ["Jack Archibald - UW Madison Computer Science"];
+var textPosition = 0;
+var speed = 100;
 typewriter = () => {
-    document.querySelector("#message").
-    innerHTML = messageArray[0].substring(0,textPosition) +
-    "<span>\u25ae</span>"
-    if(textPosition++ != messageArray[0].length){
-        setTimeout(typewriter, speed);
-    }
-}
-window.addEventListener("load", typewriter)
+  document.querySelector("#title").innerHTML =
+    messageArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
+  if (textPosition++ != messageArray[0].length) {
+    setTimeout(typewriter, speed);
+  }
+};
+window.addEventListener("load", typewriter);
